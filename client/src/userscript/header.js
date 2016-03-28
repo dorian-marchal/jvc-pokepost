098af4693211c7857fec72865258feaeafbbb9cb
dorian-marchal <marchal.dorian@gmail.com>
2016-03-28 23:41:09 +0200

    Replace webpack {css,style}-loader by userscript-css-loader

--- client/src/userscript/header.js
+++ client/src/userscript/header.js
@@ -5,5 +5,6 @@
 // @namespace   http://www.spawnkill.fr
 // @include     /https?://.*\.jeuxvideo\.com/.*/
 // @version     0.0.1
+// @grant       GM_addStyle
 // @run-at document-start
 // ==/UserScript==
