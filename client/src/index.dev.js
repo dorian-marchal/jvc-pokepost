fe49ddb67fd2ddff83181c9632375d30107ad4eb
dorian-marchal <marchal.dorian@gmail.com>
2016-04-09 10:56:41 +0200

    Expose some modules in dev build

--- client/src/index.dev.js
+++ client/src/index.dev.js
@@ -1,2 +1,8 @@
+// Expose certains modules pour faciliter les tests.
+window.Post = require('./Post');
+window.PageUtil = require('./PageUtil');
+window.UserUtil = require('./UserUtil');
+window.Pokepost = require('./Pokepost');
+
 // Charge le fichier principal.
 require('./index.js');
